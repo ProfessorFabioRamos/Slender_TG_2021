a1b92941c0fea00c985d6e88d7f9bdacef3d8bb2
Fabio Ramos <fabiocasperxan@gmail.com>
2021-10-01 07:53:18 -0300

    24-09-21

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(playerTransform.position);
+    }
+}
