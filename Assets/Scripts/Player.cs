10b92a67466ef1a9ba81675bb65fc732732d264b
Fabio Ramos <fabiocasperxan@gmail.com>
2021-10-08 08:09:28 -0300

    01-10-21

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    public Text scoreText;
     public int score = 0;
+    public Camera fpsCam;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +18,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = score+"/8";
+
+        RaycastHit hit;
+
+        Vector3 rayOrigin = fpsCam.ScreenToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+
+        if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, 2.0f))
+        {
+            if(hit.transform.tag == "Gun")
+            {
+                //INPUT
+                Destroy(hit.transform.gameObject);
+            }
+        }
     }
 
-    void OnCollisionEnter(Collision other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Collect")
         {
